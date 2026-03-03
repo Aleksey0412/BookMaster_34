c11c23a75cf33fe09e9a4fe967d851df72e58c29
Aleksey <a987gr@gmail.com>
2026-03-03 07:32:17 +0300

    Внедрение таблиц с авторами и названиями книг

--- BookMaster_34/View/Pages/BrowseCatalogPage.xaml.cs
+++ BookMaster_34/View/Pages/BrowseCatalogPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookMaster_34.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,18 @@ namespace BookMaster_34.View.Pages
     /// </summary>
     public partial class BrowseCatalogPage : Page
     {
+
+        //Создадим список для вытягивания данных из таблиц
+        private readonly List<BookAuthor> _bookAuthors;
+
         public BrowseCatalogPage()
         {
             InitializeComponent();
+
+            //Заполняем локальный список
+            _bookAuthors = App.GetContext().BookAuthors.ToList();
+
+            LoadData();
         }
 
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
@@ -33,6 +43,11 @@ namespace BookMaster_34.View.Pages
         private void PriviousPageBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void LoadData()
+        {
+            BookAutorsLv.ItemsSource = _bookAuthors;
         }
     }
 }
